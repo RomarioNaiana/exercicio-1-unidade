0d19d2b8848369a66091e7e579ceab8c5407d5af
RomarioNaiana <romarionaiana@gmail.com>
2025-04-08 18:52:55 +0000

    exercico 7 concluido

---
+++ questao7.js
@@ -0,0 +1,10 @@
+function indentificador(n1){
+    if (n1 == 0){
+        console.log ("Neutro")
+    }else if (n1 % 2== 0){
+        console.log("Par")
+    }else {
+        console.log("ímpar")
+    }
+} 
+ indentificador (0)
