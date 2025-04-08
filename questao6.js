0725c91ac5544bfcbeddfbb19d565fb7b450f8f3
RomarioNaiana <romarionaiana@gmail.com>
2025-04-08 18:38:56 +0000

    exercicio 6 finalizado

---
+++ questao6.js
@@ -0,0 +1,10 @@
+function indentificador(n1){
+    if (n1 == 0){
+        console.log ("Neutro")
+    }else if (n1 % 2== 0){
+        console.log("positivo")
+    }else (n1 <0)
+        console.log("negativo")
+}
+
+indentificador(-9)
